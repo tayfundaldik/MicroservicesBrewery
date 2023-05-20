49fa3b534b596f8a05f386e601acaf1637206245
Tayfun Daldık <79011413+tayfundaldik@users.noreply.github.com>
2023-05-20 14:05:55 +0300

    Adding beer controller.

---
+++ src/main/java/com/datwashurtt/microservicesbrewery/web/controller/BeerController.java
@@ -0,0 +1,28 @@
+package com.datwashurtt.microservicesbrewery.web.controller;
+
+import com.datwashurtt.microservicesbrewery.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/ap1/v1/beer")
+@RestController
+public class BeerController {
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
+        //todo impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+        //todo impl
+        return new ResponseEntity((HttpStatus.CREATED));
+    }
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId,@RequestBody BeerDto beerDto){
+        //todo impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
